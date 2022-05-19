0c02e0486ee7ca6d8a6b0968bfcaf6939de0ac8a
Brayan Orlando Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-05-19 16:13:02 -0500

    migrations

--- DjangoApp/DjangoApp/settings.py
+++ DjangoApp/DjangoApp/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Local apps
+    'polls.apps.PollsConfig',
+
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
