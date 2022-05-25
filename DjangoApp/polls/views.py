1d250e54c4cc91b9c266dce42f87287a98684e3b
Brayan Orlando Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-05-25 10:55:56 -0500

    views and templates

--- DjangoApp/polls/views.py
+++ DjangoApp/polls/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
-# Create your views here.
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
